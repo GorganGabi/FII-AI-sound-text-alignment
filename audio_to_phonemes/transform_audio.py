33b0edad913916320a39dacbc4ce75fc455d2c23
RMan135 <r_man135@yahoo.com>
2018-12-01 19:37:23 +0200

    +text extraction for testing purposes

--- audio_to_phonemes/transform_audio.py
+++ audio_to_phonemes/transform_audio.py
@@ -3,10 +3,17 @@ import sys
 from pocketsphinx import AudioFile, get_model_path, get_data_path
 
 
+help = """Arguments for console usage:
+-h\tShow this
+-p <file_path>\tExtract phonemes
+-w <file_path>\tExtract words
+"""
+
+
 def get_phonemes_from_file(file_path):
     """
     :param file_path: audio file (must be raw 16khz 16bit)
-    :return: a list of phrases
+    :return: a list of phrases made of phonemes
     """
 
     model_path = get_model_path()
@@ -19,8 +26,6 @@ def get_phonemes_from_file(file_path):
         'no_search': False,
         'full_utt': False,
         'hmm': os.path.join(model_path, 'en-us'),
-        #'lm': os.path.join(model_path, 'en-us.lm.bin'),
-        #'dict': os.path.join(model_path, 'cmudict-en-us.dict'),
         'allphone': os.path.join(model_path, 'en-us/en-us-phone.lm.dmp'),
         'beam': 1e-20,
         'pbeam': 1e-20,
@@ -37,10 +42,62 @@ def get_phonemes_from_file(file_path):
     return phrases
 
 
+def get_words_from_file(file_path):
+    """
+    :param file_path: audio file (must be raw 16khz 16bit)
+    :return: a list of phrases made of words
+    """
+
+    model_path = get_model_path()
+    data_path = get_data_path()
+
+    config = {
+        'verbose': False,
+        'audio_file': file_path,
+        'buffer_size': 2048,
+        'no_search': False,
+        'full_utt': False,
+        'hmm': os.path.join(model_path, 'en-us'),
+        'lm': os.path.join(model_path, 'en-us.lm.bin'),
+        'dict': os.path.join(model_path, 'cmudict-en-us.dict')
+    }
+
+    audio = AudioFile(**config)
+
+    phrases = []
+
+    for phrase in audio:
+        phrases.append(str(phrase))
+
+    return phrases
+
+
 if __name__ == "__main__":
 
     if len(sys.argv) > 1:
-        if os.path.exists(sys.argv[1]) and os.path.isfile(sys.argv[1]):
-            phrases = get_phonemes_from_file(sys.argv[1])
+
+        if sys.argv[1] == '-h':
+            print(help)
+            exit(0)
+
+    if len(sys.argv) > 2:
+        f = None
+
+        if sys.argv[1] == '-p':
+            f = get_phonemes_from_file
+        if sys.argv[1] == '-w':
+            f = get_words_from_file
+
+        if f is None:
+            exit(0)
+
+        if os.path.exists(sys.argv[2]) and os.path.isfile(sys.argv[2]):
+            phrases = f(sys.argv[2])
             for phrase in phrases:
                 print(phrase)
+        else:
+            print("Invalid file.")
+
+        exit(0)
+
+    print("No command given. Use the -h argument to view available commands.")
