20bb748a8a7eddae159e19e54172849f86cf2b22
emabalica <ema.balica@yahoo.com>
2018-12-02 19:03:52 +0200

    Update Table.py

--- Aliniere texte/Table.py
+++ Aliniere texte/Table.py
@@ -18,7 +18,7 @@ def Build_Table(text1, text2, gap_penalty, mismatch_penalty):
         table[0][j]=j*gap_penalty
     for i in range(1,n+1):
         for j in range(1,m+1):
-            if t1[i-1]==t2[j-1]:
+            if text1[i-1]==text2[j-1]:
                 table[i][j]=table[i-1][j-1]+1
             else:
                 table[i][j] = max(table[i - 1][j - 1] + mismatch_penalty, table[i - 1][j] + gap_penalty, table[i][j-1] + gap_penalty)
