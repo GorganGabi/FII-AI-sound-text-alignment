f6608bd504f7f3771804b7719e774296c78baa79
emabalica <ema.balica@yahoo.com>
2018-12-03 18:48:30 +0200

    Update text_alignment.py

--- Aliniere texte/text_alignment.py
+++ Aliniere texte/text_alignment.py
@@ -16,18 +16,18 @@ def build_table(text1, text2, gap_penalty, mismatch_penalty):
         table.append(row)
 
     for i in range(n + m + 1):
-        table[i][0] = i * pgap
+        table[i][0] = i * gap_penalty
     for j in range(n + m + 1):
-        table[0][j] = j * pgap
+        table[0][j] = j * gap_penalty
 
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             if text1[i - 1] == text2[j - 1]:
                 table[i][j] = table[i - 1][j - 1] + 1
             else:
-                table[i][j] = max(table[i - 1][j - 1] + pxy,
-                                  table[i - 1][j] + pgap,
-                                  table[i][j - 1] + pgap)
+                table[i][j] = max(table[i - 1][j - 1] + mismatch_penalty,
+                                  table[i - 1][j] + gap_penalty,
+                                  table[i][j - 1] + gap_penalty)
 
     # reconstructie
     max_length = n + m
@@ -46,20 +46,20 @@ def build_table(text1, text2, gap_penalty, mismatch_penalty):
             ypos = ypos - 1
             i = i - 1
             j = j - 1
-        elif table[i - 1][j - 1] + pxy == table[i][j]:
+        elif table[i - 1][j - 1] + mismatch_penalty == table[i][j]:
             xans[xpos] = text1[i - 1]
             yans[ypos] = text2[j - 1]
             xpos = xpos - 1
             ypos = ypos - 1
             i = i - 1
             j = i - 1
-        elif table[i - 1][j] + pgap == table[i][j]:
+        elif table[i - 1][j] + gap_penalty == table[i][j]:
             xans[xpos] = text1[i - 1]
             yans[ypos] = '_'
             xpos = xpos - 1
             ypos = ypos - 1
             i = i - 1
-        elif table[i][j - 1] + pgap == table[i][j]:
+        elif table[i][j - 1] + gap_penalty == table[i][j]:
             xans[xpos] = '_'
             yans[ypos] = text2[j - 1]
             xpos = xpos - 1
