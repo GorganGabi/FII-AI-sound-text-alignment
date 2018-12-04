e04adb58ec118457d33558e85d8002d8a3de05c3
Daniel GORGAN <dgorgan@bitdefender.com>
2018-12-04 10:04:47 +0200

    added relative path to py module

--- Server/Controllers/upload.controller.js
+++ Server/Controllers/upload.controller.js
@@ -1,53 +1,53 @@
 const multer = require('multer')
 const path = require('path')
-const spawn = require("child_process").spawn
-const config = require ("../src/config").default
+const spawn = require("child_process").spawn;
+const config = require("../src/config").default;
 
 
 module.exports.uploading = (req, res) => {
 
-  const storage = multer.diskStorage({
-    destination: './uploads',
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-    }
-  })
+    const storage = multer.diskStorage({
+        destination: './uploads',
+        filename: function (req, file, cb) {
+            cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+        }
+    });
 
-  function checkFileType (file, cb) {
-    const filetypes = /mp3|wav/
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
-    let mimetype
-    (file.mimetype === 'audio/mpeg' || file.mimetype === 'audio/wav') ? mimetype = true : mimetype = false
-    if (mimetype && extname) {
-      return cb(null, true)
-    } else {
-      cb('Eroare: Doar imagini!')
+    function checkFileType(file, cb) {
+        const filetypes = /mp3|wav/;
+        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+        let mimetype;
+        (file.mimetype === 'audio/mpeg' || file.mimetype === 'audio/wav') ? mimetype = true : mimetype = false;
+        if (mimetype && extname) {
+            return cb(null, true)
+        } else {
+            cb('Eroare: Doar imagini!')
+        }
     }
-  }
 
-  const upload = multer({
-    storage: storage,
-    fileFilter: function (req, file, cb) {
-      checkFileType(file, cb)
-    }
-  }).single('mySound')
+    const upload = multer({
+        storage: storage,
+        fileFilter: function (req, file, cb) {
+            checkFileType(file, cb)
+        }
+    }).single('mySound');
 
-  upload(req, res, (err) => {
-    if (err) {
-      const error = {error: 'Fisierul nu s-a putut incarca...Extensie gresita. Uploadati din nou!'};
-      get_sound_alignment_result(error, res)
-    }
-    else {
-      const result = {upload: req.file}
-      get_sound_alignment_result(result, res)
-    }
-  })
-}
+    upload(req, res, (err) => {
+        if (err) {
+            const error = {error: 'Fisierul nu s-a putut incarca...Extensie gresita. Uploadati din nou!'};
+            get_sound_alignment_result(error, res)
+        }
+        else {
+            const result = {upload: req.file};
+            get_sound_alignment_result(result, res)
+        }
+    })
+};
 
 function get_sound_alignment_result(input, res) {
     // CALL PYTHON MODULE TO GET THE RESULT
-    input = JSON.stringify(input)
-    const pythonProcess = spawn("py", [config.sound_alignment_file_path, input])
+    input = JSON.stringify(input);
+    const pythonProcess = spawn("py", [path.join(process.cwd(), 'dummy.py'), input]);
     let response = "";
 
     pythonProcess.stdout.on("data", (data) => {
