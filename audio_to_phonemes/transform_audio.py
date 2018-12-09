20ad85cbda0cc7ddbbcfbefcac9810bf9549a588
RMan135 <r_man135@yahoo.com>
2018-12-09 16:25:00 +0200

    +support for custom phonetic language models

--- audio_to_phonemes/transform_audio.py
+++ audio_to_phonemes/transform_audio.py
@@ -10,13 +10,16 @@ help = """Arguments for console usage:
 -h\tShow this
 -p <file_path>\tExtract phonemes
 -w <file_path>\tExtract words
+
+Example: transform_audio.py -p sample1.raw -model=it -d
 """
 
 
-def get_phonemes_from_file(file_path, detailed=False):
+def get_phonemes_from_file(file_path, detailed=False, model='en-us'):
     """
     :param file_path: path to audio file (must be raw 16khz 16bit)
-    :param detailed: False - return only phonemes; True - return tuples (phoneme, start_frame, end_frame)
+    :param detailed: False - (default) return only phonemes; True - return tuples (phoneme, start_frame, end_frame)
+    :param model: specify language model
     :return: a list of phrases made of phonemes/tuples
     """
 
@@ -29,8 +32,8 @@ def get_phonemes_from_file(file_path, detailed=False):
         'buffer_size': 2048,
         'no_search': False,
         'full_utt': False,
-        'hmm': os.path.join(model_path, 'en-us'),
-        'allphone': os.path.join(model_path, 'en-us/en-us-phone.lm.dmp'),
+        'hmm': os.path.join(model_path, model),
+        'allphone': os.path.join(model_path, 'phone.lm.dmp'),
         'beam': 1e-20,
         'pbeam': 1e-20,
         'lw': 2.0
@@ -110,12 +113,17 @@ if __name__ == "__main__":
         if f is None:
             exit(0)
 
+        model='en-us'
+        for arg in sys.argv:
+            if arg.startswith('-model='):
+                model = arg.split('=')[1]
+
         detailed=False
         if '-d' in sys.argv:
             detailed=True
 
         if os.path.exists(sys.argv[2]) and os.path.isfile(sys.argv[2]):
-            phrases = f(sys.argv[2], detailed=detailed)
+            phrases = f(sys.argv[2], detailed=detailed, model=model)
             for phrase in phrases:
                 print(phrase)
         else:
