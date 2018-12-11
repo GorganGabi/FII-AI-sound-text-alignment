de9c402bfd9fe3d29d2593e05dcc4b3f4f1c6352
Daniel GORGAN <dgorgan@bitdefender.com>
2018-12-11 14:13:52 +0200

    unit tests

--- Server/src/config.js
+++ Server/src/config.js
@@ -1,23 +1,25 @@
-import Joi from 'joi'
+// import Joi from 'joi'
 
 require('dotenv').config();
 
-const envVarsSchema = Joi.object({
-    NODE_ENV: Joi.string()
-        .default('development'),
-    SERVER_PORT: Joi.number()
-        .default(8080),
-    DB_HOST: Joi.string().required()
-        .description('DB host'),
-}).unknown()
-    .required();
-
-const {error, value: envVars} = Joi.validate(process.env, envVarsSchema);
+// const envVarsSchema = Joi.object({
+//     NODE_ENV: Joi.string()
+//         .default('development'),
+//     SERVER_PORT: Joi.number()
+//         .default(8080),
+//     DB_HOST: Joi.string().required()
+//         .description('DB host'),
+// }).unknown()
+//     .required();
+//
+// const {error, value: envVars} = Joi.validate(process.env, envVarsSchema);
 
 const config = {
-    env: envVars.NODE_ENV,
-    port: envVars.SERVER_PORT,
-    sound_alignment_file_path: envVars.SOUND_ALIGNMENT_FILE_PATH
+    env: process.env.NODE_ENV,
+    port: process.env.SERVER_PORT,
+    sound_alignment_file_path: process.env.SOUND_ALIGNMENT_FILE_PATH
 };
 
-export default config
+module.exports= {
+    config:config
+}
