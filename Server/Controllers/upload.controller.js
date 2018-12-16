83f6ff6d62f08a747f09b25a0959c99e66d38d9b
Daniel GORGAN <dgorgan@bitdefender.com>
2018-12-16 20:11:25 +0200

    refactored file saving (overwriting)

--- Server/Controllers/upload.controller.js
+++ Server/Controllers/upload.controller.js
@@ -9,7 +9,7 @@ module.exports.uploading = (req, res) => {
     const storage = multer.diskStorage({
         destination: './uploads',
         filename: function (req, file, cb) {
-            cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+            cb(null, 'mySound'+ path.extname(file.originalname))
         }
     });
 
