4bf3ab2a29b7acb95353dfed2a59634625e2830f
Andrei Maxim <andreey_047@yahoo.com>
2018-12-17 23:45:29 +0200

    stripAudio call

--- Server/Controllers/upload.controller.js
+++ Server/Controllers/upload.controller.js
@@ -5,6 +5,7 @@ const path = require('path')
 const spawn = require("child_process").spawn;
 const config = require("../src/config").default;
 const fs = require('fs');
+const async = require("async");
 
 
 var root;
@@ -44,46 +45,83 @@ module.exports.uploading = (req, res) => {
     upload(req, res, (err) => {
         if (err) {
             const error = {error: 'Fisierul nu s-a putut incarca...Extensie gresita. Uploadati din nou!'};
-            get_sound_alignment_result(error, res)
+            res.send(error);
+            return;
         }
         else {
             //fisierul salvat o sa fie mySound.extensie (vezi filename din multer.diskStorage)
             // dupa care trebuie trimis modulului de prelucrare date
             // si sa fie convertit in test.raw, and that's all, ar trebui sa mearga dupa
+            // console.log(req);
             console.log('[SERVER] Am primit in urma upload-ului fisierul:');
             console.log(req.file);
             console.log('[SERVER] Am primit in textul:');
             console.log(req.body.text);
+            console.error(req.body);
             fs.writeFile(path.join(process.cwd(), 'uploads/text.txt'), req.body.text, function (err) {
                 if (err) {
                     return console.log(err);
                 }
                 console.log("[SERVER] Textul a fost salvat cu success in text.txt!");
-                get_sound_alignment_result(res)
+                get_sound_alignment_result(res, req)
             });
 
         }
     })
 };
 
-function get_sound_alignment_result(res) {
+function get_sound_alignment_result(res, req) {
     // CALL PYTHON MODULE TO GET THE RESULT
-    console.log('[SERVER] Trimitem ca parametru fisierul primit catre modulul:' + path.join(root, 'text_alignment/text_alignment.py'));
-    const pythonProcess = spawn("py",
-        [path.join(path.join(root, 'text_alignment/text_alignment.py')),
-            '-rec', path.join(path.join(process.cwd(), '/uploads/test.raw')), //argv [1] si argv[2]
-            '-orig', path.join(path.join(process.cwd(), '/uploads/text.txt')), //argv [3] si argv[4]
-            '-out', path.join(path.join(process.cwd(), '/uploads/response.txt')) //argv [5] si argv[6]
-        ]);
-    let response = "";
-    pythonProcess.stdout.on("end", () => {
-        console.log('[SERVER] Am primit de la modulul de python urmatorul raspuns:');
-        console.log(response);
-        fs.readFile(path.join(path.join(process.cwd(), '/uploads/response.txt')), function (err, data) {
-            if (err) {
-                console.log(err)
-            }
-            res.json({syncData: JSON.parse(data.toString())})
-        });
+    async.waterfall([
+        (cb) => {
+            // SEND AUDIO FILE TO `PRELUCRAREA DATELOR` MODULE
+            console.log('[SERVER] Trimitem ca parametru fisierul primit catre modulul:' + path.join(root, 'Prelucrarea datelor/stripAudio.py'));
+            const pythonProcess = spawn("py",
+                [path.join(path.join(root, 'Prelucrarea datelor/stripAudio.py')),
+                    '/uploads/mySound.mp3',
+                    '/uploads/test.raw'
+                ]);
+            let response = "";
+            pythonProcess.stdout.on("data", (data) => {
+                response += data;
+            });
+            pythonProcess.stdout.on("end", () => {
+                console.log("StripAudio finishied. Response is: ", response);
+                cb();
+            });
+        },
+        (cb) => {
+            // SEND RAW FILE + TEXT FILE TO text_alignment module
+            console.log('[SERVER] Trimitem ca parametru fisierul primit catre modulul:' + path.join(root, 'text_alignment/text_alignment.py'));
+            const pythonProcess = spawn("py",
+                [path.join(path.join(root, 'text_alignment/text_alignment.py')),
+                    '-rec', path.join(path.join(process.cwd(), '/uploads/test.raw')), //argv [1] si argv[2]
+                    '-orig', path.join(path.join(process.cwd(), '/uploads/text.txt')), //argv [3] si argv[4]
+                    '-out', path.join(path.join(process.cwd(), '/uploads/response.txt')) //argv [5] si argv[6]
+                ]);
+            let response = "";
+            pythonProcess.stdout.on("data", (data) => {
+                response += data;
+            });
+            pythonProcess.stdout.on("end", () => {
+                console.log('[SERVER] Am primit de la modulul de python urmatorul raspuns:');
+
+                fs.readFile(path.join(path.join(process.cwd(), '/uploads/response.txt')), function (err, data) {
+                    if (err) {
+                        console.log(err)
+                        return cb(err);
+                    }
+                    data = JSON.parse(data);
+                    console.log(data);
+                    cb(null, data);
+                });
+            });
+        }
+    ], (err, data) => {
+        if (err) {
+            return res.send("eroare sefu");
+        }
+        res.json({syncData: data});
     });
+
 }
