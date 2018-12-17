978c7dd8c48f0ddabfba1d86f556a216e88c300d
Daniel GORGAN <dgorgan@bitdefender.com>
2018-12-17 10:48:36 +0200

    call text_alignment script

--- Server/Controllers/upload.controller.js
+++ Server/Controllers/upload.controller.js
@@ -4,14 +4,20 @@ const multer = require('multer')
 const path = require('path')
 const spawn = require("child_process").spawn;
 const config = require("../src/config").default;
+const fs = require('fs');
 
+
+var root;
+getRootPath(process.cwd(), (res) => {
+    root = res
+});
 
 module.exports.uploading = (req, res) => {
 
     const storage = multer.diskStorage({
         destination: './uploads',
         filename: function (req, file, cb) {
-            cb(null, 'mySound'+ path.extname(file.originalname))
+            cb(null, 'mySound' + path.extname(file.originalname))
         }
     });
 
@@ -20,7 +26,7 @@ module.exports.uploading = (req, res) => {
         const filetypes = /mp3|wav/;
         const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
         let mimetype;
-        (file.mimetype === 'audio/mpeg' || file.mimetype === 'audio/wav'|| file.mimetype === 'audio/mp3') ? mimetype = true : mimetype = false;
+        (file.mimetype === 'audio/mpeg' || file.mimetype === 'audio/wav' || file.mimetype === 'audio/mp3') ? mimetype = true : mimetype = false;
         if (mimetype && extname) {
             return cb(null, true)
         } else {
@@ -45,24 +51,31 @@ module.exports.uploading = (req, res) => {
             console.log(req.file);
             console.log('[SERVER] Am primit in textul:');
             console.log(req.body.text);
-            const result = {upload: req.file};
-            get_sound_alignment_result(result, res)
+            fs.writeFile(path.join(process.cwd(), 'uploads/text.txt'), req.body.text, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("[SERVER] Textul a fost salvat cu success in text.txt!");
+                get_sound_alignment_result(res)
+            });
+
         }
     })
 };
 
-function get_sound_alignment_result(input, res) {
+function get_sound_alignment_result(res) {
     // CALL PYTHON MODULE TO GET THE RESULT
-    input = JSON.stringify(input);
-    let root
-    getRootPath(process.cwd(), (res) => {
-        root = res
-    });
     console.log('[SERVER] Trimitem ca parametru fisierul primit catre modulul:' + path.join(root, 'text_alignment/text_alignment.py'));
-    const pythonProcess = spawn("py", [path.join(path.join(root, 'text_alignment/text_alignment.py')), input]);
+    const pythonProcess = spawn("py",
+        [path.join(path.join(root, 'text_alignment/text_alignment.py')),
+            '-rec', path.join(path.join(process.cwd(), '/uploads/test.raw')),
+            '-orig', path.join(path.join(process.cwd(), '/uploads/text.txt')),
+            '-out', path.join(path.join(process.cwd(), '/uploads/response.txt'))
+        ]);
     let response = "";
 
     pythonProcess.stdout.on("data", (data) => {
+        console.log(data.toString())
         response += data
     });
     pythonProcess.stdout.on("error", (err) => {
@@ -71,10 +84,7 @@ function get_sound_alignment_result(input, res) {
 
     pythonProcess.stdout.on("end", () => {
         console.log('[SERVER] Am primit de la modulul de python urmatorul raspuns:');
-        if(response) {
-            response = JSON.parse(response);
-        }
         console.log(response);
-        res.json(response)
+        res.json({response: response})
     });
 }
