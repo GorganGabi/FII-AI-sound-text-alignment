4dc497878d486f28d75e957b90b2f9f7d78bf465
V. Vlad <gladladvlad@yandex.com>
2018-12-17 23:12:41 +0200

    +added error checking and automatic extension detection

--- Prelucrarea datelor/stripAudio.py
+++ Prelucrarea datelor/stripAudio.py
@@ -1,5 +1,11 @@
+import os
 import sox
 
+
+def getFileExtension(inFilePath):
+    extension = os.path.splitext(inFilePath)[1][1:]
+
+    return extension
 
 '''
 Arguments:
@@ -10,6 +16,16 @@ Arguments:
         extension (which must be '.raw').
 '''
 def convertFileToRaw(inFilePath, outFilePath):
-    tfm = sox.Transformer();
-    tfm.set_output_format(file_type='raw', rate=16000, bits=16, channels=1)
-    tfm.build(inFilePath, outFilePath)
+    try:
+        tfm = sox.Transformer();
+
+
+        fileType = getFileExtension(outFilePath)
+
+        tfm.set_output_format(file_type = fileType, rate = 16000, bits = 16, channels = 1)
+
+        tfm.build(inFilePath, outFilePath)
+
+    except Exception as e:
+        print(e)
+        return
