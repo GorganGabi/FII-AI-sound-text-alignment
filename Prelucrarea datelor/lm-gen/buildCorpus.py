7f27c89ddd57693d732516624286e58b3241c942
V. Vlad <gladladvlad@yandex.com>
2018-12-17 20:28:34 +0200

    +added functionality for getting the remote wikipedia archive

---
+++ Prelucrarea datelor/lm-gen/buildCorpus.py
@@ -0,0 +1,5 @@
+import httpDownloader as httpd
+import constants
+
+httpd.downloadHTTP(constants.roWikiArchiveUrl)
+
---
+++ Prelucrarea datelor/lm-gen/constants.py
@@ -0,0 +1,1 @@
+roWikiArchiveUrl = "https://dumps.wikimedia.org/rowiki/latest/rowiki-latest-pages-articles.xml.bz2"
